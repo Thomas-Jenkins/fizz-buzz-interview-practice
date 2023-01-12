101987a48a461ba3494299819210cd4a69c77e1d
Thomas Jenkins <107004915+Thomas-Jenkins@users.noreply.github.com>
2023-01-12 09:45:44 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// For this assignment I know I will need a for loop to check each number from 1 to 100. I will need three functions, One to check if the number is divisable by
+// 3 which will result in fizz being printed to the screen, one to check if the number is divisable by 5 which will result in buzz being printed to the screen 
+// and finally one to check if the number is divisable by both 3 and 5 which will result in fizzbuzz being printed tot he screen. If none of those are true, then 
+// the number itself will be printed tot he screen.
+
+
